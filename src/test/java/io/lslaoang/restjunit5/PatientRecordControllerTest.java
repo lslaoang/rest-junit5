ac2fd4a0f5d3b093f6d3b63b349ea92f32ffd35f
Laurel Lao Ang <laurellaoang@gmail.com>
2021-10-31 12:26:38 +0800

    Mockito added

--- src/test/java/io/lslaoang/restjunit5/PatientRecordControllerTest.java
+++ src/test/java/io/lslaoang/restjunit5/PatientRecordControllerTest.java
@@ -4,10 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.lslaoang.restjunit5.controller.PatientRecordController;
 import io.lslaoang.restjunit5.domain.PatientRecord;
 import io.lslaoang.restjunit5.repository.PatientRecordRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(PatientRecordController.class)
 public class PatientRecordControllerTest {
@@ -25,5 +39,18 @@ public class PatientRecordControllerTest {
     PatientRecord RECORD_2 = new PatientRecord(2l, "David Landup", 27, "New York USA");
     PatientRecord RECORD_3 = new PatientRecord(3l, "Jane Doe", 31, "New York USA");
 
+    @Test
+    public void getAllUser_succes() throws Exception {
+        List<PatientRecord> patientRecordList = new ArrayList<>(Arrays.asList(RECORD_1, RECORD_2, RECORD_3));
 
+        Mockito.when(patientRecordRepository.findAll()).thenReturn(patientRecordList);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/patient")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect((ResultMatcher) jsonPath("$[2].name", is("Jane Doe")));
+
+    }
 }
