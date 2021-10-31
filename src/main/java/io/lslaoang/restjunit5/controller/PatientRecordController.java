4ff82815369c96b61e5155483c7cf1c81bd34c19
Laurel Lao Ang <laurellaoang@gmail.com>
2021-10-31 13:18:35 +0800

    Validated test 3/4

--- src/main/java/io/lslaoang/restjunit5/controller/PatientRecordController.java
+++ src/main/java/io/lslaoang/restjunit5/controller/PatientRecordController.java
@@ -1,10 +1,10 @@
 package io.lslaoang.restjunit5.controller;
 
 import io.lslaoang.restjunit5.domain.PatientRecord;
-import io.lslaoang.restjunit5.exception.InvalidRequestException;
 import io.lslaoang.restjunit5.repository.PatientRecordRepository;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -29,27 +29,34 @@ public class PatientRecordController {
 
     @PostMapping
     public PatientRecord createRecord(@RequestBody @Valid PatientRecord patientRecord){
-        return patientRecordRepository.save(patientRecord);
+        //Validation
+        if(patientRecord == null || patientRecord.getPatientId()==null){
+            throw new InvalidRequestException("Patient Record or Patient ID must not be null!");
+        }else{
+            return patientRecordRepository.save(patientRecord);
+        }
     }
 
     @PutMapping
-    public PatientRecord updatePatientRecord(PatientRecord patientRecord) throws NotFoundException {
+    public PatientRecord updatePatientRecord(@RequestBody PatientRecord patientRecord) throws NotFoundException {
 
         //Validation
         if(patientRecord == null || patientRecord.getPatientId()==null){
             throw new InvalidRequestException("Patient Record or Patient ID must not be null!");
         }
-
         Optional<PatientRecord> optionalPatientRecord = patientRecordRepository.findById(patientRecord.getPatientId());
-        if(optionalPatientRecord.isEmpty()){
+        if(optionalPatientRecord.isEmpty() || !optionalPatientRecord.isPresent()){
             throw new NotFoundException("Patient with ID " + patientRecord.getPatientId() + " does not exist.");
         }
-        PatientRecord existingPatientRecord = optionalPatientRecord.get();
-        existingPatientRecord.setName(patientRecord.getName());
-        existingPatientRecord.setAddress(patientRecord.getAddress());
-        existingPatientRecord.setAge(patientRecord.getAge());
+        else {
+            PatientRecord existingPatientRecord = optionalPatientRecord.get();
+            existingPatientRecord.setName(patientRecord.getName());
+            existingPatientRecord.setAddress(patientRecord.getAddress());
+            existingPatientRecord.setAge(patientRecord.getAge());
 
-        return patientRecordRepository.save(existingPatientRecord);
+            return patientRecordRepository.save(existingPatientRecord);
+        }
+
     }
 
     @DeleteMapping(value = "{patientId}")
@@ -59,6 +66,13 @@ public class PatientRecordController {
             throw new NotFoundException("Patient ID "+patientId +" does not exist.");
         }
         patientRecordRepository.deleteById(patientId);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public class InvalidRequestException extends RuntimeException {
+        public InvalidRequestException(String s) {
+            super(s);
+        }
     }
 
 }
--- src/test/java/io/lslaoang/restjunit5/PatientRecordControllerTest.java
+++ src/test/java/io/lslaoang/restjunit5/PatientRecordControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.lslaoang.restjunit5.controller.PatientRecordController;
 import io.lslaoang.restjunit5.domain.PatientRecord;
 import io.lslaoang.restjunit5.repository.PatientRecordRepository;
+import javassist.NotFoundException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +12,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -45,12 +48,112 @@ public class PatientRecordControllerTest {
 
         Mockito.when(patientRecordRepository.findAll()).thenReturn(patientRecordList);
 
+        //Test 1
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/patient")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(3)))
-                .andExpect((ResultMatcher) jsonPath("$[2].name", is("Jane Doe")));
+                .andExpect(jsonPath("$[2].name", is("Jane Doe")));
 
+        //Test 2
+        Mockito.when(patientRecordRepository.findById(RECORD_1.getPatientId())).thenReturn(java.util.Optional.of(RECORD_1));
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/patient/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.name", is("Rayven Yor")));
+
+
+    }
+
+    @Test
+    public void createRecord_success() throws Exception {
+        PatientRecord record = PatientRecord.builder()
+                .name("John Doe")
+                .age(47)
+                .address("New York USA")
+                .build();
+
+        Mockito.when(patientRecordRepository.save(record)).thenReturn(record);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/patient")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(record));
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.name", is("John Doe")));
+    }
+
+    @Test
+    public void updatePatientRecord_success() throws Exception {
+        PatientRecord updatedRecord = PatientRecord.builder()
+                .patientId(1l)
+                .name("Rayven Zambo")
+                .age(23)
+                .address("Cebu Philippines")
+                .build();
+
+        Mockito.when(patientRecordRepository.findById(RECORD_1.getPatientId())).thenReturn(Optional.of(RECORD_1));
+        Mockito.when(patientRecordRepository.save(updatedRecord)).thenReturn(updatedRecord);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/patient")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(updatedRecord));
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.name", is("Rayven Zambo")));
+    }
+
+    @Test
+    public void updatePatientRecord_nullId() throws Exception {
+        PatientRecord updatedRecord = PatientRecord.builder()
+                .name("Sherlock Holmes")
+                .age(40)
+                .address("221B Baker Street")
+                .build();
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/patient")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(updatedRecord));
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isBadRequest())
+                .andExpect(result ->
+                        assertTrue(result.getResolvedException() instanceof PatientRecordController.InvalidRequestException))
+                .andExpect(result ->
+                        assertEquals("Patient Record or Patient ID must not be null!", result.getResolvedException().getMessage()));
+    }
+
+    @Test
+    public void updatePatientRecord_recordNotFound() throws Exception {
+        PatientRecord updatedRecord = PatientRecord.builder()
+                .patientId(5l)
+                .name("Sherlock Holmes")
+                .age(40)
+                .address("221B Baker Street")
+                .build();
+
+        Mockito.when(patientRecordRepository.findById(updatedRecord.getPatientId())).thenReturn(null);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/patient")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(updatedRecord));
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isBadRequest())
+                .andExpect(result ->
+                        assertTrue(result.getResolvedException() instanceof NotFoundException))
+                .andExpect(result ->
+                        assertEquals("Patient with ID 5 does not exist.", result.getResolvedException().getMessage()));
     }
 }
