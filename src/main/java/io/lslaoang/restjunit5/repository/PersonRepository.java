c0476cfb3bfa6edba08e768db166015860db46f8
Laurel Lao Ang <laurellaoang@gmail.com>
2021-11-06 09:10:05 +0800

    @NoRepositoryBean added to PersonRepository

--- src/main/java/io/lslaoang/restjunit5/repository/PersonRepository.java
+++ src/main/java/io/lslaoang/restjunit5/repository/PersonRepository.java
@@ -3,6 +3,7 @@ package io.lslaoang.restjunit5.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
+//Annotation to help ComponentScanner classify this interface as non-bean
 @NoRepositoryBean
 public interface PersonRepository<Object, Long> extends JpaRepository <Object, Long>{
 }
