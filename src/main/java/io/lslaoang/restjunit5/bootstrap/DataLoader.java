623dc14ed90de0d421c7f086cb6b7343f148c328
Laurel Lao Ang <laurellaoang@gmail.com>
2021-11-12 14:03:29 +0800

    surgeon added

--- src/main/java/io/lslaoang/restjunit5/bootstrap/DataLoader.java
+++ src/main/java/io/lslaoang/restjunit5/bootstrap/DataLoader.java
@@ -14,14 +14,21 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Doctor doctor = new Doctor();
-
         doctor.setDoctorId(001L);
         doctor.setDoctorName("Laurel L");
         doctor.setDoctorAge(47);
         doctor.setDoctorAddress("Makati, Philippines");
 
         doctorRepository.save(doctor);
-
         System.out.println("Doctor persisted");
+
+        Doctor surgeon = new Doctor();
+        surgeon.setDoctorId(002L);
+        surgeon.setDoctorName("Laoang L");
+        surgeon.setDoctorAge(23);
+        surgeon.setDoctorAddress("New York, USA");
+
+        doctorRepository.save(surgeon);
+        System.out.println("Surgeon persisted");
     }
 }
