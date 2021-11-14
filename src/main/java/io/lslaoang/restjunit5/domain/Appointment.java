e62194a7e7358468e1fc3f1ae2dbe8e0242375c6
Laurel Lao Ang <laurellaoang@gmail.com>
2021-11-14 10:23:00 +0800

    Table Relationship updated

--- src/main/java/io/lslaoang/restjunit5/domain/Appointment.java
+++ src/main/java/io/lslaoang/restjunit5/domain/Appointment.java
@@ -1,13 +1,20 @@
 package io.lslaoang.restjunit5.domain;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
 @Entity
 public class Appointment {
 
+    @Id
+    Long appointmentId;
+
+    @OneToOne
+    @JoinColumn(name = "doctor_in_charge_doctor_id")
     Doctor doctorInCharge;
+
+    @OneToMany
     List<PatientRecord> patientRecordList;
     LocalDate appointmentTime;
     String location;
@@ -16,31 +23,4 @@ public class Appointment {
         return doctorInCharge;
     }
 
-    public void setDoctorInCharge(Doctor doctorInCharge) {
-        this.doctorInCharge = doctorInCharge;
-    }
-
-    public List<PatientRecord> getPatientRecordList() {
-        return patientRecordList;
-    }
-
-    public void setPatientRecordList(List<PatientRecord> patientRecordList) {
-        this.patientRecordList = patientRecordList;
-    }
-
-    public LocalDate getAppointmentTime() {
-        return appointmentTime;
-    }
-
-    public void setAppointmentTime(LocalDate appointmentTime) {
-        this.appointmentTime = appointmentTime;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
 }
