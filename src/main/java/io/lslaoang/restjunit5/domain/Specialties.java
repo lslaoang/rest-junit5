494700a2a444a1955ba8aeef68302c8dea732e14
Laurel Lao Ang <laurellaoang@gmail.com>
2021-11-17 18:57:47 +0800

    Enum for Specialties added

---
+++ src/main/java/io/lslaoang/restjunit5/domain/Specialties.java
@@ -0,0 +1,11 @@
+package io.lslaoang.restjunit5.domain;
+
+public enum Specialties {
+    EENT,
+    Plastic_SURGEON,
+    General_Physician,
+    Neurosurgeon,
+    Psychology,
+    Pediatrician,
+    Gynecology,
+}
